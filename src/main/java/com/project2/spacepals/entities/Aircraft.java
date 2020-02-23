e0e75adb5e9fd4fdb898bf3431d321f834fb9d22
Bosco <nzibosco@gmail.com>
2020-02-23 18:55:59 -0500

    removed unnecessary details in aircraft entity

--- src/main/java/com/project2/spacepals/entities/Aircraft.java
+++ src/main/java/com/project2/spacepals/entities/Aircraft.java
@@ -1,12 +1,9 @@
 package com.project2.spacepals.entities;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
-import org.hibernate.annotations.Fetch;
-import org.hibernate.annotations.FetchMode;
 
 import javax.persistence.*;
 import java.io.Serializable;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
 
@@ -15,18 +12,17 @@ import java.util.Objects;
 @Table(name = "AIRCRAFTS")
 public class Aircraft implements Serializable {
 
-    @Id @Column
+    @Id
+    @Column
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
 
     @Column(nullable = false, unique = true)
     private String name;
 
-    @Column(nullable = false, unique = true)
-    private double rate;
 
     @JoinColumn
-    @ManyToOne(cascade={
+    @ManyToOne(cascade = {
             CascadeType.REMOVE, CascadeType.DETACH,
             CascadeType.MERGE, CascadeType.PERSIST
     })
@@ -44,38 +40,22 @@ public class Aircraft implements Serializable {
     @Enumerated(EnumType.STRING)
     private Capacity capacity;
 
-    @Enumerated(EnumType.STRING)
-    private RentalStatus status;
 
     public Aircraft() {
         super();
     }
 
-    public Aircraft(String name, double rate, Capacity capacity, Company owner, RentalStatus status) {
+    public Aircraft(String name, Capacity capacity, Company owner) {
         this.name = name;
-        this.rate = rate;
         this.owner = owner;
         this.capacity = capacity;
-        this.status = status;
     }
 
-    public Aircraft(String name, double rate, Company owner, List<Flight> flights, Capacity capacity, RentalStatus status) {
-        this.name = name;
-        this.rate = rate;
-        this.owner = owner;
-        //this.flights = flights;
-        this.capacity = capacity;
-        this.status = status;
-    }
-
-    public Aircraft(int id, String name, double rate, Company owner, List<Flight> flights, Capacity capacity, RentalStatus status) {
+    public Aircraft(int id, String name, Company owner, Capacity capacity) {
         this.id = id;
         this.name = name;
-        this.rate = rate;
         this.owner = owner;
-        //this.flights = flights;
         this.capacity = capacity;
-        this.status = status;
     }
 
     public int getId() {
@@ -94,14 +74,6 @@ public class Aircraft implements Serializable {
         this.name = name;
     }
 
-    public double getRate() {
-        return rate;
-    }
-
-    public void setRate(double rate) {
-        this.rate = rate;
-    }
-
     public Company getOwner() {
         return owner;
     }
@@ -118,56 +90,29 @@ public class Aircraft implements Serializable {
         this.capacity = capacity;
     }
 
-    public RentalStatus getStatus() {
-        return status;
-    }
-
-    public void setStatus(RentalStatus status) {
-        this.status = status;
-    }
-
-//    public List<Flight> getFlights() {
-//        return flights;
-//    }
-//
-//    public void setFlights(List<Flight> flights) {
-//        this.flights = flights;
-//    }
-//
-//    public void addFlight(Flight flight) {
-//        if(flights == null) flights = new ArrayList<>();
-//        flights.add(flight);
-//    }
-
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
+        if (!(o instanceof Aircraft)) return false;
         Aircraft aircraft = (Aircraft) o;
-        return id == aircraft.id &&
-                Double.compare(aircraft.rate, rate) == 0 &&
-                Objects.equals(name, aircraft.name) &&
-                Objects.equals(owner, aircraft.owner) &&
-                //Objects.equals(flights, aircraft.flights) &&
-                capacity == aircraft.capacity &&
-                status == aircraft.status;
+        return getId() == aircraft.getId() &&
+                Objects.equals(getName(), aircraft.getName()) &&
+                Objects.equals(getOwner(), aircraft.getOwner()) &&
+                getCapacity() == aircraft.getCapacity();
     }
 
-//    @Override
-//    public int hashCode() {
-//        return Objects.hash(id, name, rate, owner, flights, capacity, status);
-//    }
-//
+    @Override
+    public int hashCode() {
+        return Objects.hash(getId(), getName(), getOwner(), getCapacity());
+    }
+
     @Override
     public String toString() {
         return "Aircraft{" +
                 "id=" + id +
                 ", name='" + name + '\'' +
-                ", rate=" + rate +
                 ", owner=" + owner +
-                //", flights=" + flights +
                 ", capacity=" + capacity +
-                ", status=" + status +
                 '}';
     }
 }
